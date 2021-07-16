7469fc90937dc54ef5a1f0774c7461eaefa60f9e
KamiVela17 <87462486+KamiVela17@users.noreply.github.com>
2021-07-16 12:13:17 -0600

    +se agregaron comentarios

--- src/main/java/com/mycompany/tarea1/progra2/Main.java
+++ src/main/java/com/mycompany/tarea1/progra2/Main.java
@@ -13,18 +13,21 @@ import java.util.Scanner;
  * @author Karla Flores
  */
 public class Main {
-
+//Creando constantes del precio de la divisas
     static final double dolares = 7.75;
     static final double euros = 9.16;
     static final double yuanes = 1.20;
 
     public static void main(String[] args) {
 
+        //utilizacion del import scanner para la lectura de datos en la consola
         Scanner sc = new Scanner(System.in);
 
+        //Creacion de variables a utilizar
         String nombre1, nombre2, apellido1, apellido2;
         double res, valor = 0;
 
+        //Obtencion de datos
         System.out.println("Ingrese su primer nombre: ");
 
         nombre1 = sc.nextLine();
@@ -39,6 +42,10 @@ public class Main {
 
         System.out.println("Ingrese la cantidad que desea convertir: ");
         valor = sc.nextDouble();
+        
+        
+        //creacion de un menu para la seleccion de la divisa a la que se desea
+        //convertir
         boolean salir = false;
         int opcion; //Guardaremos la opcion del usuario
 
@@ -57,6 +64,7 @@ public class Main {
 
                 switch (opcion) {
                     case 1:
+                        //dolares
                         System.out.println("Se ha seleccionado Quetzales a Dolares");
                         res = valor * dolares;
                         System.out.println("Su nombre es: " + nombre1 + ' ' + nombre2 + ' ' + apellido1 + ' ' + apellido2);
@@ -64,12 +72,14 @@ public class Main {
 
                         break;
                     case 2:
+                        //euros
                         System.out.println("Se ha seleccionado Quetzales a Euros");
                         res = valor * euros;
                         System.out.println("Su nombre es: " + nombre1 + ' ' + nombre2 + ' ' + apellido1 + ' ' + apellido2);
                         System.out.println("El resultado es: " + res);
                         break;
                     case 3:
+                        //yuanes
                         System.out.println("Se ha seleccionado Quetzales a Yuanes");
                         res = valor * yuanes;
                         System.out.println("Su nombre es: " + nombre1 + ' ' + nombre2 + ' ' + apellido1 + ' ' + apellido2);
